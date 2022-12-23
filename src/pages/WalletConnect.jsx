0c6448af1915ee3e0528e21f7e489549a1896ade
Lotpite <exstornado@gmail.com>
2022-12-23 17:26:44 +0200

    disconnect wallet func added

--- src/pages/WalletConnect.jsx
+++ src/pages/WalletConnect.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import img1 from '../assets/images/icon/connect-1.png'
-import { buyNft, createEthWallet } from '../services/metamaskApi';
+import { connectNewWallet, disconnectCurrentWallet } from '../services/metamaskApi';
 // import { buyNft } from '../services/ethersWalletService';
 // import { connectWallet } from '../services/metamaskApi';
 
@@ -22,15 +22,26 @@ const WalletConnect = () => {
     const [wallet, setWallet] = useState('')
 
     useEffect(() => {
+        const wallet_AccessToken = localStorage.getItem('wallet_AccessToken')
+        setWallet(wallet_AccessToken)
     }, [])
 
-    const connectNewWallet = async ( ) => {
-        const newWallet = await window.ethereum.request({ method: "eth_requestAccounts" })
-        if(newWallet) {
-            alert('Metamask is connected')
-        }
-        setWallet(newWallet[0])
+    const connectWallet = () => {
+        setWallet(connectNewWallet())
+        
     }
+
+    const disconnectWallet = () => {
+        setWallet(disconnectCurrentWallet())
+    }
+
+    // const connectNewWallet = async ( ) => {
+    //     const newWallet = await window.ethereum.request({ method: "eth_requestAccounts" })
+    //     if(newWallet) {
+    //         alert('Metamask is connected')
+    //     }
+    //     setWallet(newWallet[0])
+    // }
 
     return (
         <div>
@@ -69,13 +80,19 @@ const WalletConnect = () => {
                             <div className="sc-box-icon-inner style-2">
                                 {
                                     data.map((item,index) => (
-                                        <div key={index} className="sc-box-icon" onClick={() => connectNewWallet()}>
+                                        <div key={index} className="sc-box-icon">
                                             <div className="img">
                                                 <img src={item.img} alt="Axies" />
                                             </div>
                                             {/* <h4 className="heading"><Link to="/login">{item.title}</Link> </h4> */}
                                             <h4 className="heading" >{item.title}</h4>
                                             <p className="content">{item.description}</p>
+                                            
+                                            <a className='sc-button header-slider style style-1 wallet fl-button pri-1' onClick={() => wallet ? disconnectWallet() : connectWallet() }>
+                                                <span>
+                                                    {wallet ? 'disconnect' : 'connect'}
+                                                </span>
+                                            </a>
                                          </div>
                                     ))
                                 }
--- src/services/metamaskApi.js
+++ src/services/metamaskApi.js
@@ -7,35 +7,60 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 import Web3 from "web3";
 import Web3Modal from "web3modal";
 
-let web3;
-let accounts = [];
+// let web3;
+// let accounts = [];
 
-  const providerOptions = {
-    walletconnect: {
-      package: WalletConnectProvider,
-      options: {
-        network: "matic", // here
-        rpc: {
-          137: "https://polygon-rpc.com"
-        }
-      }
+//   const providerOptions = {
+//     walletconnect: {
+//       package: WalletConnectProvider,
+//       options: {
+//         network: "matic", // here
+//         rpc: {
+//           137: "https://polygon-rpc.com"
+//         }
+//       }
+//     }
+//   };
+
+
+//   const web3Modal = new Web3Modal({
+//     network: "matic", // optional
+//     cacheProvider: false, // optional
+//     providerOptions // required
+//   });
+
+
+//   export const connectWallet = async () => {
+//     web3Modal.clearCachedProvider();
+//     const provider = await web3Modal.connect();
+//     web3 = new Web3(provider);
+//     accounts = await web3.eth.getAccounts();
+//     console.log(accounts, "connected");
+//     web3.eth.defaultAccount = accounts[0];
+//   };
+
+  export const connectNewWallet = async ( ) => {
+    const newWallet = await window.ethereum.request({ method: "eth_requestAccounts" })
+    if(newWallet) {
+        alert('Metamask is connected')
     }
-  };
+    localStorage.setItem('wallet_AccessToken', newWallet[0])
+    return newWallet
+  }
 
-  const web3Modal = new Web3Modal({
-    network: "matic", // optional
-    cacheProvider: false, // optional
-    providerOptions // required
-  });
-
-  export const connectWallet = async () => {
-    web3Modal.clearCachedProvider();
-    const provider = await web3Modal.connect();
-    web3 = new Web3(provider);
-    accounts = await web3.eth.getAccounts();
-    console.log(accounts, "connected");
-    web3.eth.defaultAccount = accounts[0];
-  };
+  export const disconnectCurrentWallet = () => {
+      try {
+        const wallet_AccessToken = localStorage.getItem('wallet_AccessToken')
+        if (wallet_AccessToken) {
+          localStorage.removeItem('wallet_AccessToken')
+          alert('Wallet is disconnected')
+          console.log(wallet_AccessToken);
+          return undefined
+        }
+      } catch (error) {
+        alert('Something wen wrong', error)
+      }
+  }
 
     export const createEthWallet = () => {
         const web3 = new Web3(window.ethereum)
@@ -46,19 +71,25 @@ let accounts = [];
 
     export async function buyNft( orderId='ETHEREUM:0x19f487016770542dc6137b06499a4f7b42c9580f12d85d6347964b03b7682143') {
        try {
-          const ethWallet = createEthWallet()
+          const wallet_AccessToken = localStorage.getItem('wallet_AccessToken')
+          if(wallet_AccessToken) {
+            const ethWallet = createEthWallet()
+            const sdk = createRaribleSdk(ethWallet, "prod")
+        
+            // Get order info
+            const buy = await sdk.order.buy.prepare({                           
+            orderId: toOrderId(orderId) 
+            })
 
-          const sdk = createRaribleSdk(ethWallet, "prod")
-      
-          // Get order info
-          const buy = await sdk.order.buy.prepare({                           
-          orderId: toOrderId(orderId) 
-          })
-
-          // Send transaction
-          const result = await buy.submit({
-            amount: 1,    
-          })
+            // Send transaction
+            const result = await buy.submit({
+              amount: 1,    
+            })
+          }
+          if(!wallet_AccessToken) {
+            alert('please connect the wallet')
+          }
+          
        } catch (error) {
           alert(error)
        }
